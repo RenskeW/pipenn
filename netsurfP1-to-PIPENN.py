bd432e72a0f9c0fc811ac5ff24ad9c1a1ca2aa02
AGoetzee <arthurgoetzee@outlook.com>
2022-07-18 20:02:09 +0200

    Added loading function

--- netsurfP1-to-PIPENN.py
+++ netsurfP1-to-PIPENN.py
@@ -1,5 +1,21 @@
 import pandas as pd
-import numpy as np
-import io
 
-print(pd.read_csv('netsurf_example_output.txt', comment='#').head())
+PIPENN_COLS = ['class', 'AA', 'name', 'number', 'rel_surf_acc', 'abs_surf_acc', 'z', 'prob_helix', 'prob_sheet',
+               'prob_coil']
+
+
+def load_netsurfp_output(path: str = 'netsurfp_output.txt') -> pd.DataFrame:
+    """Loads the data in the correct format.
+    TODO Check if Reza wanted these column names
+
+    :rtype: DataFrame
+    :param path: str - Path to NetsurfP output
+    :return: NetsurfP output in DataFrame format
+    """
+    df = pd.read_fwf(path, comment='#', header=None, names=PIPENN_COLS, infer_nrows=10000)
+    return df
+
+
+if __name__ == '__main__':
+    # TODO add argparser to specify netsurfP output path
+    df = load_netsurfp_output('netsurfp_output.txt')
